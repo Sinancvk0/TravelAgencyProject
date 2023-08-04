3b50b19c713dfd039cde5095d84de21aa815626f
sinan <sinancivak00@gmail.com>
2023-08-05 02:28:28 +0300

    Feature model Create

---
+++ TraversalCoreProject/EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string  Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string İmage { get; set; }
+        public bool Status { get; set; }
+    }
+}
