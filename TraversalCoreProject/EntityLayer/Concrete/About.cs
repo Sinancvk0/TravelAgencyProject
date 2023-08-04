0bcfaf4dd3228cefe11c8c71e5b47d1f7d894a51
sinan <sinancivak00@gmail.com>
2023-08-05 02:22:41 +0300

    About model created

---
+++ TraversalCoreProject/EntityLayer/Concrete/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public bool Status { get; set; }
+    }
+}
