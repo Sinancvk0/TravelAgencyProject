3340297ad58dd193b2181fafae83138970ca7d3b
sinan <sinancivak00@gmail.com>
2023-08-05 04:01:53 +0300

    Context class eklendi

---
+++ TraversalCoreProject/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=TraversalDB;Integrated Security=True;TrustServerCertificate=True;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> About2s { get; set; }
+        public DbSet<Contect> Contects { get; set; }
+        public DbSet<Destination> MyProperty { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Feature2> Feature2s { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
