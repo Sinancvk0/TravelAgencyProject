832851b2c18cf958cb4f11bc339f8d57ca09c65d
sinan <sinan@SINANCIVAK>
2023-08-05 06:25:44 +0300

    .

---
+++ TraversalCoreProject/BussinessLayer/Abstract/IGenericServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Abstract
+{
+    public interface IGenericServices<T>
+
+    {
+        void TDelete(T t);
+        void TAdd(T t);
+        void TUpdate(T t);
+        List<T> GetList();
+        T GetByID(int id);
+    }
+}
