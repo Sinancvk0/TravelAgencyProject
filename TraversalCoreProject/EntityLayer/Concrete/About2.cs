b193276bbe19a2e94b1943be5934b8f70a062e4a
sinan <sinan@SINANCIVAK>
2023-08-05 04:02:19 +0300

    primary key  eklendi

--- TraversalCoreProject/EntityLayer/Concrete/About2.cs
+++ TraversalCoreProject/EntityLayer/Concrete/About2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class About2
     {
+        [Key]
         public int About2ID { get; set; }
         public string Title1 { get; set; }
         public string Title2 { get; set; }
