4e8f478c4d8467285290f56de174f576ac665e95
sinan <sinancivak00@gmail.com>
2023-08-05 20:38:52 +0300

    Views Componetler oluşturuldu Thema Türkçeleştirilmeye Başlandı

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _SliderPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+
+        }
+    }
+}
