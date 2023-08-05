54ddb9febe744ee61f4d43aca97b13a093392345
sinan <sinan@SINANCIVAK>
2023-08-05 04:35:43 +0300

    Repository Oluşturuldu

---
+++ TraversalCoreProject/DataAccessLayer/Abstract/IGuideDal.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGuideDal
+    {
+        void Insert(Guide guide);
+        void Delete(Guide guide);
+
+        void Update(Guide guide);
+        List<Guide> GetList();
+    }
+}
