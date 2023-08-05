31f2188f647de9c732343b81035fdef38ebb5c2b
sinan <sinan@SINANCIVAK>
2023-08-05 22:56:27 +0300

    destinations componet eklendi

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_PopularDestinationsPartial.cs
@@ -0,0 +1,18 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _PopularDestinationsPartial:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+           
+            return View(values);
+        }
+    }
+}
