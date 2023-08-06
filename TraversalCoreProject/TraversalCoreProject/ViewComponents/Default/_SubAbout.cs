e4438a2b9848a011623d243bbaac0274238f9afd
sinan <sinancivak00@gmail.com>
2023-08-06 06:35:27 +0300

    Thema Giydirme ve Menü Türkçeleştirme  Yapıldı

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,18 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _SubAbout:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+
+        {
+            SubAboutManager subAboutManager = new SubAboutManager(new EfSubAboutDal());
+            var values=subAboutManager.TGetList();
+            return View(values);
+
+        }
+    }
+}
---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Testimonial:ViewComponent
+    {
+        TestimonialManager _testimonial = new TestimonialManager(new EfTestimonialDal());
+        public IViewComponentResult Invoke()
+        {
+          
+            var values=_testimonial.TGetList();
+            return View(values);
+        }
+    }
+}
