2aead77038b1160821bb2e60a6f4c98869dabc4d
sinan <sinan@SINANCIVAK>
2023-08-06 06:35:27 +0300

    Thema Giydirme ve Menü Türkçeleştirme  Yapıldı

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,17 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            //var values = featureManager.TGetList();
+
+            return View();
+        }
+    }
+}
