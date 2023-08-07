be735cf64c3beaef1fbfab3a24c3f5cf587ef36b
sinan <sinan@SINANCIVAK>
2023-08-07 19:54:10 +0300

    logincontroller oluşturuldu

---
+++ TraversalCoreProject/TraversalCoreProject/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    [AllowAnonymous]//Altında bulunduğu tüm kodları geçerli bütün kurallardan muaf tutuyor
+
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
