ccf6d3c0af802ed652f73e7a308911123ce6982b
sinan <sinancivak00@gmail.com>
2023-08-07 09:14:53 +0300

    Commed controller oluşturuldu

---
+++ TraversalCoreProject/TraversalCoreProject/Controllers/CommetController.cs
@@ -0,0 +1,26 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+public class CommetController : Controller
+{
+    CommentManager _commentManager = new CommentManager(new EfCommentDal());
+
+    [HttpGet]
+    public PartialViewResult AddComment()
+    {
+        return PartialView();
+    }
+
+    [HttpPost]
+    public IActionResult AddComment(Comment p)
+    {
+        p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+        p.CommentState = true;
+        _commentManager.TAdd(p);
+
+        return RedirectToAction("Index", "Destination");
+    }
+}
