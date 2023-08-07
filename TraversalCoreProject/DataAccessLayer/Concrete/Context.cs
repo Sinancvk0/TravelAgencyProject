724778d4d6564c8985a9a0b6763c71a97e1eb854
sinan <sinan@SINANCIVAK>
2023-08-07 05:34:30 +0300

    Destination ID ye göre Kullanıcı yorumları getirme işlemi için repository method yazıldı comment için işlemler yapıldı

--- TraversalCoreProject/DataAccessLayer/Concrete/Context.cs
+++ TraversalCoreProject/DataAccessLayer/Concrete/Context.cs
@@ -19,6 +19,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<Newsletter> Newsletters { get; set; }
         public DbSet<SubAbout> SubAbouts { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
     }
 }
