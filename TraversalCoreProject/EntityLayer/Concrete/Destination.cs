34a6dafee18c84e9f0d17550d3e47f0865407ff4
sinan <sinan@SINANCIVAK>
2023-08-07 05:35:15 +0300

    comment modeli oluşturuldu Destibnation ile bire çok ilişki yapıldı

--- TraversalCoreProject/EntityLayer/Concrete/Destination.cs
+++ TraversalCoreProject/EntityLayer/Concrete/Destination.cs
@@ -24,5 +24,6 @@ namespace EntityLayer.Concrete
         public string Details1 { get; set; }
         public string Details2 { get; set; }
         public string Image2 { get; set; }
+        public List<Comment> Comments { get; set; }
     }
 }
