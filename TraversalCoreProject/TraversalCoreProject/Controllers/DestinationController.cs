283c874d6e014eb4a487293acf579dd5d6f75d12
sinan <sinan@SINANCIVAK>
2023-08-07 03:39:55 +0300

    Destination Details İşlemlerine Başlandı

---
+++ TraversalCoreProject/TraversalCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,31 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            var values=destinationManager.TGetByID(id);
+            return View(values);
+
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
+
+        }
+    }
+}
