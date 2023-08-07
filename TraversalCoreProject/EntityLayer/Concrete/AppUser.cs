ca415650c4126c715eb78ce1aa76576669ad3e7f
sinan <sinancivak00@gmail.com>
2023-08-07 19:53:48 +0300

    Identity model oluşturuldu

---
+++ TraversalCoreProject/EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EntityLayer.Concrete
+{
+    public class AppUser:IdentityUser<int>
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+    }
+}
