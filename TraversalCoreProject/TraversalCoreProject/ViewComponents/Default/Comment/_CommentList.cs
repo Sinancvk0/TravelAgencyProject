1b6de22be5b11b7b9660fbb0d88167f3c30572a7
sinan <sinan@SINANCIVAK>
2023-08-07 05:36:35 +0300

    comment componetleri oluşturudu

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/Comment/_CommentList.cs
@@ -0,0 +1,16 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default.Comment
+{
+    public class _CommentList:ViewComponent
+    {
+        CommentManager commentManager =new CommentManager(new EfCommentDal());
+        public IViewComponentResult Invoke(int id)
+        {
+           var values= commentManager.TGetDestinationByID(id);
+            return View(values);
+        }
+    }
+}
