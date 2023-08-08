a205248f647c2b6aeb28ca140ef2c93270827cd3
sinan <sinan@SINANCIVAK>
2023-08-08 22:12:28 +0300

    Area controllerlar oluşturuldu

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,20 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
