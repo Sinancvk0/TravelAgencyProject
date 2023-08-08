03752916078d6b35405cbd03c9e232cb31a409ee
sinan <sinancivak00@gmail.com>
2023-08-08 22:12:28 +0300

    Area controllerlar oluşturuldu

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Member/Controllers/CommentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    public class CommentController : Controller
+    {
+        [Area("Member")]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
