18cfff3787b93c359eddef6cd83b21c3beb9b49a
sinan <sinan@SINANCIVAK>
2023-08-08 22:13:40 +0300

    area endpoints eklendi

--- TraversalCoreProject/TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/TraversalCoreProject/Startup.cs
@@ -68,6 +68,14 @@ namespace TraversalCoreProject
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
         }
     }
 }
