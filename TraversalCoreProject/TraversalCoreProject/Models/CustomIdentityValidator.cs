2657e487a476de2a082c9a11f59cb813257091aa
sinan <sinan@SINANCIVAK>
2023-08-08 07:23:15 +0300

    Identity model classları kişislleştirildi ve şifre  uyarı Türkçeleştirme yapıldı

---
+++ TraversalCoreProject/TraversalCoreProject/Models/CustomIdentityValidator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TraversalCoreProject.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Parola minimum {length} karakter olmalıdır"
+
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresDigit),
+                Description = "Şifreler en az bir rakam içermelidir."
+            };
+        }
+
+    }
+
+}
+
---
+++ TraversalCoreProject/TraversalCoreProject/Models/UserRegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Adınızı Giriniz")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen SoyAdınızı Giriniz")]
+        public string SurName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adınızı Giriniz")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Mail Giriniz")]
+        public string Mail   { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifrenizi Giriniz")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifrenizi Tekrar Giriniz")]
+        [Compare("Password",ErrorMessage ="Şifreler Uyumlu Değil")]
+        public string ConfirmPassword { get; set; }
+    }
+}
