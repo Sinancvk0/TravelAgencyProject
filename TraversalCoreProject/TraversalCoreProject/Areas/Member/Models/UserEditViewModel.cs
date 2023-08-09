889c92c581b834406d97200801cca693f9f19c25
sinan <sinan@SINANCIVAK>
2023-08-09 03:32:22 +0300

    Update için Area/Useredit model oluşturuldu

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Member/Models/UserEditViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TraversalCoreProject.Areas.Member.Models
+{
+    public class UserEditViewModel
+    {
+        public string name { get; set; }
+        public string surName { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+        public string phonenumber { get; set; }
+        public string mail { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile Image {  get; set; } //Bir HTML formundan gelen dosyaları işlemek için kullanılır. 
+
+    }
+}
