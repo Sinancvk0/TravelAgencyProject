c065bc24eb17e2f23e3bdbb2efd1dacf5c38846d
sinan <sinancivak00@gmail.com>
2023-08-12 01:00:11 +0300

    ProfileInformation componetler oluşturuldu ve partialviev olarak ayrıldı

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.Default.MemberDashboard
+{
+    public class _ProfileInformation:ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task< IViewComponentResult >InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.Surname;
+            ViewBag.phone = values.PhoneNumber;
+            ViewBag.email=values.Email;
+            ViewBag.Location = "Türkiye";
+            return View();
+
+
+        }
+    }
+}
