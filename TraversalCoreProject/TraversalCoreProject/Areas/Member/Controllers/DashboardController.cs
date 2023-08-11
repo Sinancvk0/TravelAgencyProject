5124dc1ee65e6ee505bb337c323f3a28faa6c6a8
sinan <sinancivak00@gmail.com>
2023-08-12 00:58:40 +0300

    Dashboard controller Oluşturuldu

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class DashboardController : Controller
+    {
+        private readonly   UserManager<AppUser>_userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            var values=await _userManager.FindByNameAsync(User.Identity.Name);  
+
+            ViewBag.userName=values.Name+" "+values.Surname;
+            ViewBag.ImageUser = values.ImageUrl;
+            
+            return View();
+        }
+    }
+}
