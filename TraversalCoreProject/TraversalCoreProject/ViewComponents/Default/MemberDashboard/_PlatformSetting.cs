3492319cd132db34fa445884f243a58fb1092d17
sinan <sinancivak00@gmail.com>
2023-08-12 03:36:39 +0300

    ProfileSetting eklendi

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,16 @@
+﻿using BussinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default.MemberDashboard
+{
+    public class _PlatformSetting:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+         
+
+            return View();
+
+        }
+    }
+}
