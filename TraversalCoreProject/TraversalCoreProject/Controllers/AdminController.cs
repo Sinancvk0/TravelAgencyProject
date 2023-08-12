15e33f17907b285290d3a5032acaae84803f916a
sinan <sinancivak00@gmail.com>
2023-08-13 02:44:21 +0300

    AdminLayout Partiallar oluşturuldu

--- TraversalCoreProject/TraversalCoreProject/Controllers/AdminController.cs
+++ TraversalCoreProject/TraversalCoreProject/Controllers/AdminController.cs
@@ -12,5 +12,26 @@ namespace TraversalCoreProject.Controllers
         {
             return PartialView();
         }
+        public PartialViewResult PartialSideBar()
+        {
+
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+
+            return PartialView();
+        }
+
     }
 }
