d1c31a83e4032783b5b57c95acaa870743c1223f
sinan <sinancivak00@gmail.com>
2023-08-14 07:50:39 +0300

    Comment Controller Güncellendi

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,32 @@
+﻿using BussinessLayer.Abstract;
+using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public IActionResult Index()
+        {
+            var values=_commentService.TGetCommentWithDestination();
+            return View(values);
+        }
+
+        public IActionResult Delete( int id)
+        {
+            var values=_commentService.TGetByID(id);
+             _commentService.TDelete(values);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
