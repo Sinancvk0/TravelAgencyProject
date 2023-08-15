3106a671071412403cc90366041336b97bb8ca68
sinan <sinancivak00@gmail.com>
2023-08-15 20:03:10 +0300

    Excel ve Pdf raporlama işlemleri

---
+++ TraversalCoreProject/BussinessLayer/ValidationRules/GuideValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.ValidationRules
+{
+
+    public class GuideValidator : AbstractValidator<Guide>
+    {
+        public GuideValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Lütfen Ad Soyad Giriniz");
+            RuleFor(x => x.Descripition).NotEmpty().WithMessage("Lütfen Açıklama Giriniz");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Lütfen Rehber Görselini Giriniz");
+
+        }
+
+    }
+}
---
+++ TraversalCoreProject/TraversalCoreProject/Controllers/PdfReportController.cs
@@ -0,0 +1,73 @@
+﻿
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StaticPdfReport()
+        {
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PdfReports/" + "dosya1.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+            document.Open();
+            Paragraph paragraph = new Paragraph("Traversal Rezervasyon Pdf Raporu");
+
+            document.Add(paragraph);
+            document.Close();
+
+            return File("/PdfReports/dosya1.pdf", "application/pdf", "dosya1.pdf");
+
+
+
+
+        }
+
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PdfReports/" + "dosya2.pdf");
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                Document document = new Document(PageSize.A4);
+                PdfWriter writer = PdfWriter.GetInstance(document, stream);
+                document.Open();
+
+                PdfPTable pdfPTable = new PdfPTable(3);
+
+                pdfPTable.AddCell("Misafir Adı");
+                pdfPTable.AddCell("Misafir Soyad");
+                pdfPTable.AddCell("Misafir TC");
+
+                pdfPTable.AddCell("Sinan");
+                pdfPTable.AddCell("Çıvak");
+                pdfPTable.AddCell("12313211313");
+
+                pdfPTable.AddCell("Feyyaz");
+                pdfPTable.AddCell("Çıvak");
+                pdfPTable.AddCell("12313211313");
+
+                pdfPTable.AddCell("Emre");
+                pdfPTable.AddCell("Çıvak");
+                pdfPTable.AddCell("12313211313");
+
+                document.Add(pdfPTable);
+                document.Close();
+            }
+
+            return File("/PdfReports/dosya2.pdf", "application/pdf", "dosya2.pdf");
+        }
+
+    }
+}
