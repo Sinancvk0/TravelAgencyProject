73fd2ffed5d286ad42f25e410e12c6585abdb5bf
sinan <sinancivak00@gmail.com>
2023-08-15 07:47:00 +0300

    Guide scoped eklendi

---
+++ TraversalCoreProject/BussinessLayer/Container/ExtensionsDb.cs
@@ -0,0 +1,32 @@
+﻿using BussinessLayer.Abstract;
+using BussinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BussinessLayer.Container
+{
+    public static class ExtensionsDb
+    {
+        public static void ContainerDependencies(IServiceCollection services)
+        {
+            services.AddScoped<ICommentService, CommentManager>();
+            services.AddScoped<ICommentDal, EfCommentDal>();
+
+            services.AddScoped<IDestinationService, DestinationManager>();
+            services.AddScoped<IDestinationDal, EFDestinationDal>();
+
+            services.AddScoped<IAppUserService, AppUserManager>();
+            services.AddScoped<IAppUserDal, EfAppUserDal>();
+           
+            services.AddScoped<IReservationService, ReservationManager>();  
+            services.AddScoped<IReservationDal, EfReservationDal>();
+
+            services.AddScoped<IGuideService, GuideManager>();  
+           services.AddScoped<IGuideDal, EfGuideDal>(); 
+
+
+
+        }
+    }
+}
