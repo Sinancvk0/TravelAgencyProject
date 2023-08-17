705c709b261587977873da3aeccfbc0dc34350b6
sinan <sinancivak00@gmail.com>
2023-08-17 17:56:40 +0300

    Ajax işlemleri yapıldı

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,73 @@
+﻿using BussinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto.Prng;
+using System.Collections.Generic;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
+
+            return Json(jsonCity);
+
+        }
+        [HttpPost]
+        public IActionResult CityDestination(Destination destination)
+        {
+            destination.Status = true;
+            _destinationService.TAdd(destination);
+            var values = JsonConvert.SerializeObject(destination);
+
+            return Json(values);
+        }
+
+        public IActionResult GetById(int DestinationID)
+        {
+            var values=_destinationService.TGetByID(DestinationID);
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+
+
+        }
+
+        
+        public IActionResult Delete(int id)
+        {
+            var values = _destinationService.TGetByID(id);
+            _destinationService.TDelete(values);
+            return Ok();
+
+        }
+        public IActionResult Update(Destination destination)
+        {
+    
+            _destinationService.TUpdate(destination);
+            var v= JsonConvert.SerializeObject(destination);
+            return Json(v);
+
+
+        }
+
+
+    }
+
+}
+
