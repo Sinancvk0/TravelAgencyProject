78159d60d728435ff3b0d2aa83a7644405b27427
sinan <sinancivak00@gmail.com>
2023-08-18 14:47:34 +0300

    DTO katmanı oluşturuldu

---
+++ TraversalCoreProject/DTOLayer/DTOs/AppUserDTOs/AppUserLoginDTOs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.DTOs.AppUserDTOs
+{
+    public class AppUserLoginDTOs
+    {
+   
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ TraversalCoreProject/DTOLayer/DTOs/CityDTOs/CİtyAddDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.DTOs.CityDTOs
+{
+    public class CİtyAddDTO
+    {
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+        public string CityCountry { get; set; }
+
+    }
+}
