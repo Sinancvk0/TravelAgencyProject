d4f1032fd5b4862b3503ba52708fdee097c92b9d
sinan <sinancivak00@gmail.com>
2023-08-19 10:20:49 +0300

    Announscement Validasyon işlemleri

---
+++ TraversalCoreProject/BussinessLayer/ValidationRules/AnnouscementUpdateValidator.cs
@@ -0,0 +1,19 @@
+﻿using DTOLayer.DTOs.AnnouscementDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.ValidationRules
+{
+    public class AnnouscementUpdateValidator:AbstractValidator<AnnounscementUpdateDto>
+    {
+        public AnnouscementUpdateValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Lütfen başlık değeri giriniz");
+            RuleFor(y => y.Content).NotEmpty().WithMessage("Lütfen duyuru içeriği giriniz...");
+        }
+    }
+}
---
+++ TraversalCoreProject/BussinessLayer/ValidationRules/AnnouscementValidator.cs
@@ -0,0 +1,20 @@
+﻿using DTOLayer.DTOs.AnnouscementDTOs;
+using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.ValidationRules
+{
+    public class AnnouscementValidator : AbstractValidator<AddAnnouscementDto>
+    {
+        public AnnouscementValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Lütfen başlık değeri giriniz");
+            RuleFor(y => y.Content).NotEmpty().WithMessage("Lütfen duyuru içeriği giriniz...");
+        }
+    }
+}
