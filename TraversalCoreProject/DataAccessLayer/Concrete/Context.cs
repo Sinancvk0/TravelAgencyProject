d2fb3a5e601efddf6a64eb87308ee47363b413dd
sinan <sinancivak00@gmail.com>
2023-08-19 10:17:37 +0300

    .

--- TraversalCoreProject/DataAccessLayer/Concrete/Context.cs
+++ TraversalCoreProject/DataAccessLayer/Concrete/Context.cs
@@ -23,6 +23,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
