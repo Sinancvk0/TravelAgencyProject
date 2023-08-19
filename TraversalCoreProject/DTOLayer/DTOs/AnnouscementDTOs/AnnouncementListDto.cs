ded2dca99a84f41e2a7d101cef4ecab31c1d6f22
sinan <sinancivak00@gmail.com>
2023-08-19 10:18:27 +0300

    ANnouncement Dto Update add  list modellleri

---
+++ TraversalCoreProject/DTOLayer/DTOs/AnnouscementDTOs/AnnouncementListDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.DTOs.AnnouscementDTOs
+{
+    public class AnnouncementListDto
+    {
+        public int AnnouncemntID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ TraversalCoreProject/DTOLayer/DTOs/AnnouscementDTOs/AnnounscementUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLayer.DTOs.AnnouscementDTOs
+{
+    public class AnnounscementUpdateDto
+    {
+        public int AnnouncemntID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
