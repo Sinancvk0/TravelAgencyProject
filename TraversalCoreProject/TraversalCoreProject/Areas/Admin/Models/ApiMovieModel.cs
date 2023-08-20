47841370d568984568a71913e97548c405bd7192
sinan <sinancivak00@gmail.com>
2023-08-20 20:59:39 +0300

    Model and Views

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Admin/Models/ApiMovieModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TraversalCoreProject.Areas.Admin.Models
+{
+    public class ApiMovieModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+
+        public string rating { get; set; }
+        public int year { get; set; }
+
+        public string trailer { get; set; }
+
+    }
+}
