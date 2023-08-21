0f222b606acd2780ebde5276a66c695192c2c6d2
sinan <sinancivak00@gmail.com>
2023-08-21 20:47:24 +0300

    CQRS dizayn pattern projeye eklendi command işlemleri

---
+++ TraversalCoreProject/TraversalCoreProject/CQRS/Commands/DestinationCommands/CreateDestinationCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace TraversalCoreProject.CQRS.Commands.DestinationCommands
+{
+    public class CreateDestinationCommand
+    {
+        public int DestinationID { get; set; }
+        public string City { get; set; }
+        public string DayNight { get; set; }
+        public double Price { get; set; }
+        public int Capacity { get; set; }
+        public bool Status { get; set; }
+    }
+}
