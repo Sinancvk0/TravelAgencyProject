916de21abd4cc3261c2efcb164700cb35fe94a8c
sinan <sinancivak00@gmail.com>
2023-08-21 20:48:56 +0300

    Destination CQRS dizyn pattern taşındı  Controller işlemleri

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Admin/Controllers/DestinationCQRSController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProject.CQRS.Commands.DestinationCommands;
+using TraversalCoreProject.CQRS.Handlers.DestinationHandlers;
+using TraversalCoreProject.CQRS.Queries.DestinationQuery;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class DestinationCQRSController : Controller
+    {
+        private readonly GetAllDestinationQueryHandler _getAllDestinationQueryHandler;
+        private readonly GetDestinationByIDQueryHandler _getDestinationByIDQueryHandler;
+        private readonly CreateDestinationCommandHandlers _createDestinationCommandHandlers;
+
+        public DestinationCQRSController(GetAllDestinationQueryHandler getAllDestinationQueryHandler, GetDestinationByIDQueryHandler getDestinationByIDQueryHandler, CreateDestinationCommandHandlers createDestinationCommandHandlers)
+        {
+            _getAllDestinationQueryHandler = getAllDestinationQueryHandler;
+            _getDestinationByIDQueryHandler = getDestinationByIDQueryHandler;
+            _createDestinationCommandHandlers = createDestinationCommandHandlers;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _getAllDestinationQueryHandler.Handle();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddDestination()
+
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(CreateDestinationCommand command)
+        {
+            _createDestinationCommandHandlers.Handle(command);
+            return RedirectToAction("Index");
+
+        }
+
+
+        [HttpGet]
+        public IActionResult GetDestination(int id)
+        {
+            var values = _getDestinationByIDQueryHandler.Handle(new GetDestinationByIDQuery(id));
+
+            return View(values);
+
+        }
+        //[HttpPost]
+        //public IActionResult GetDestination(GetDestinationByIDQuery)
+        //{
+
+
+        //}
+        
+    }
+}
