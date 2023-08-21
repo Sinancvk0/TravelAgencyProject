a7c20e15ce4dcf17004dd55e973a7ed2143eb4a4
sinan <sinancivak00@gmail.com>
2023-08-21 20:47:47 +0300

    CQRS dizayn pattern projeye eklendi resullt işlemleri

---
+++ TraversalCoreProject/TraversalCoreProject/CQRS/Results/DestinationResults/GetDestinationByIDQueryResult.cs
@@ -0,0 +1,11 @@
+﻿namespace TraversalCoreProject.CQRS.Results.DestinationResults
+{
+    public class GetDestinationByIDQueryResult
+    {
+        public int DesitnationID { get; set; }
+        public string City { get; set; }
+        public string Daynight { get; set; }
+        public double Price { get; set; }
+
+    }
+}
