ba6bb8b0d94c960037d375b9c6762828c6c9de2d
sinan <sinancivak00@gmail.com>
2023-08-21 20:47:10 +0300

    CQRS dizayn pattern projeye eklendi handle işlemleri

---
+++ TraversalCoreProject/TraversalCoreProject/CQRS/Handlers/DestinationHandlers/CreateDestinationCommandHandlers.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using TraversalCoreProject.CQRS.Commands.DestinationCommands;
+
+namespace TraversalCoreProject.CQRS.Handlers.DestinationHandlers
+{
+    public class CreateDestinationCommandHandlers
+    {
+        private readonly Context _context;
+
+        public CreateDestinationCommandHandlers(Context context)
+        {
+            _context = context;
+        }
+
+        public void Handle(CreateDestinationCommand command)
+        {
+            _context.Destinations.Add(new Destination
+            {
+                City = command.City,
+                Price = command.Price,
+                DayNight = command.DayNight,
+                Capacity = command.Capacity,
+                Status=true
+
+            });
+            _context.SaveChanges();
+
+
+
+        }
+    }
+}
---
+++ TraversalCoreProject/TraversalCoreProject/CQRS/Handlers/DestinationHandlers/GetDestinationByIDQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProject.CQRS.Queries.DestinationQuery;
+using TraversalCoreProject.CQRS.Results.DestinationResults;
+
+namespace TraversalCoreProject.CQRS.Handlers.DestinationHandlers
+{
+    public class GetDestinationByIDQueryHandler
+    {
+        private readonly Context _context;
+
+        public GetDestinationByIDQueryHandler(Context context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public GetDestinationByIDQueryResult Handle(GetDestinationByIDQuery query)
+        {
+            var values = _context.Destinations.Find(query.id);
+
+            return new GetDestinationByIDQueryResult
+            {
+                DesitnationID = values.DestinationID,
+                City = values.City,
+                Daynight = values.DayNight,
+                Price = values.Price
+
+            };
+
+
+        }
+    }
+}
