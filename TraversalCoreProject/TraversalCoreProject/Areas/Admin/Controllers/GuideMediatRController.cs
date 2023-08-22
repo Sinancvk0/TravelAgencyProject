3f8f34c6c0618e3b54a059c950d2d987b1faa1fd
sinan <sinancivak00@gmail.com>
2023-08-22 12:43:38 +0300

    MediatR kütüphanesi controller

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Admin/Controllers/GuideMediatRController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TraversalCoreProject.CQRS.Commands.GuideCommands;
+using TraversalCoreProject.CQRS.Queries.GuideQueries;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class GuideMediatRController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public GuideMediatRController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _mediator.Send(new GetAllGuideQuery());
+
+            return View(values);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetGuides(int id)
+        {
+            var values = await _mediator.Send(new GetGuideByIDQuery(id));
+            return View(values);
+
+        }
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddGuide(CreateGuideCommand command)
+        {
+            await _mediator.Send(command);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
