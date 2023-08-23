35731341e20d60231392d39b947ffc1bb977be1e
sinan <sinancivak00@gmail.com>
2023-08-23 20:43:28 +0300

    Tasarmsal Düzeltmeler

--- TraversalCoreProject/TraversalCoreProject/Controllers/DefaultController.cs
+++ TraversalCoreProject/TraversalCoreProject/Controllers/DefaultController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Controllers
 {
     public class DefaultController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
