718659ef72fa3291e8047a0d92946aece8048664
sinan <sinancivak00@gmail.com>
2023-08-24 18:31:44 +0300

    Tasarımsal Düzeltmeler 2

--- TraversalCoreProject/TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/TraversalCoreProject/Controllers/DestinationController.cs
@@ -1,10 +1,12 @@
 ﻿using BussinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Controllers
 {
+    [AllowAnonymous]
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EFDestinationDal());
---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Destination/_GuideDetails.cs
@@ -0,0 +1,24 @@
+﻿using BussinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Destination
+{
+    public class _GuideDetails:ViewComponent
+    {
+
+        private readonly IGuideService _guideService;
+
+        public _GuideDetails(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var values= _guideService.TGetByID(1); 
+
+            return View(values);    
+
+        }
+    }
+}
