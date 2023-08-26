2f0d5886093f5d84b808fc5217937d20e3012d3b
sinan <sinancivak00@gmail.com>
2023-08-27 02:37:36 +0300

    Api eklendi  PostgreSQL projeye dahil edildi

---
+++ TraversalCoreProject/SignalRApi/Dal/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRApi.Dal
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context>options):base(options)
+        {
+            
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
---
+++ TraversalCoreProject/SignalRApi/Dal/Visitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SignalRApi.Dal
+{
+    public enum ECity
+    {
+        İstanbul=1,
+        Ankara=2,
+        Zonguldak=3,
+        İzmir=4,
+        Adana=5
+
+
+    }
+    public class Visitor
+    {
+        public int VisitorID { get; set; }
+        public ECity City { get; set; }
+
+        public int CityVisitCount { get; set; }
+
+        public DateTime  VisitDate { get; set; }
+
+    }
+}
