f15136a6084cbad498c3495480c09635aa213f45
sinan <sinancivak00@gmail.com>
2023-08-29 23:25:42 +0300

    rol atama model

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Admin/Models/AssignRoleModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TraversalCoreProject.Areas.Admin.Models
+{
+    public class AssignRoleModel
+    {
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+
+        public bool RoleExist { get; set; }
+    }
+}
