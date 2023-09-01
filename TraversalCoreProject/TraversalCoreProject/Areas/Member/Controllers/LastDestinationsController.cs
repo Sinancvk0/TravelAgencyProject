0b1d0c1d3780dfb6a343691f514baebf18522f3e
sinan <sinancivak00@gmail.com>
2023-09-02 02:33:18 +0300

    .

---
+++ TraversalCoreProject/TraversalCoreProject/Areas/Member/Controllers/LastDestinationsController.cs
@@ -0,0 +1,11 @@
+﻿using BussinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class LastDestinationsController : Controller
+    {
+        
+    }
+}
